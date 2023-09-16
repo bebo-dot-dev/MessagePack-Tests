6fb2d4461a828e55b2d3c1634c93403a83ba88c0
Joe Lippa <joe@bebo.dev>
2023-09-16 15:34:51 +0100

    renames

--- TestApi/MessagePackObj.cs
+++ TestApi/MessagePackObj.cs
@@ -5,7 +5,7 @@ namespace TestApi;
 [DataContract]
 public sealed record MessagePackObj
 {
-    private string? _decryptedPassword;
+    private string? _password;
     
     [DataMember(Order = 0)] 
     public int Version { get; init; } = 1;
@@ -13,10 +13,10 @@ public sealed record MessagePackObj
     [DataMember(Order = 1)] 
     public string EncryptedPassword { get; init; } = null!;
 
-    public string GetPassword(EncryptionProvider encryptionProvider)
+    public string GetPassword(EncryptionProvider provider)
     {
-        if (_decryptedPassword is not null) return _decryptedPassword;
-        _decryptedPassword = encryptionProvider.Decrypt(EncryptedPassword);
-        return _decryptedPassword;
+        if (_password is not null) return _password;
+        _password = provider.Decrypt(EncryptedPassword);
+        return _password;
     }
 }
